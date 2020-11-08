c48856cc194a24713e16b1b047d0acb91fadfbb2
seungwubaek <whiteseungwu@gmail.com>
2020-11-09 00:07:02 +0900

    [Navremocon] Keep the 'Fold' or 'Hide' status when page reloaded.

--- assets/js/customs.js
+++ assets/js/customs.js
@@ -5,36 +5,42 @@ var navBtnSetHeight = $('.nav-btn-set').css('height').split('px')[0];
 var foldNavBtnHeight = 2*navBtnSetHeight+'px';
 var navRemoconSlider = $('#nav-slider');
 var navRemoconBtns = $('#nav-buttons');
-function navRemoconFoldItem() {
-    adjustNavRemoconStyle(true);
+function navRemoconFoldItem(reverse) {
+    adjustNavRemoconStyle(reverse);
     navRemoconBtns.css('height', foldNavBtnHeight);
     navRemoconSlider.css('bottom', 'calc(-('+navRemoconSlider.outerHeight()+'px - '+foldNavBtnHeight+' - '+$('#nav-fold-upper-button').outerHeight()+'px))');
     $('#nav-fold-arraw-upper').addClass('nav-arrow-up');
     $('#nav-fold-arraw-upper').removeClass('nav-arrow-down');
     navRemoconSlider.addClass('fold');
+    localStorage.setItem('navremocon-fold', JSON.stringify(true));
 }
-function navRemoconUnfoldItem() {
+function navRemoconUnfoldItem(reverse) {
     navRemoconBtns.css('height', navRemoconBtns.children().length*navBtnSetHeight+'px');
     navRemoconSlider.css('bottom', 0);
-    adjustNavRemoconStyle(true);
+    adjustNavRemoconStyle(reverse);
     $('#nav-fold-arraw-upper').removeClass('nav-arrow-up');
     $('#nav-fold-arraw-upper').addClass('nav-arrow-down');
     navRemoconSlider.removeClass('fold');
+    localStorage.setItem('navremocon-fold', JSON.stringify(false));
 }
-function navRemoconHide() {
+function navRemoconHide(reverse) {
+    adjustNavRemoconStyle(reverse);
     var remocon = $('#nav-remocon');
     remocon.css('bottom', -(remocon.outerHeight() - $('#nav-fold-upper-button').outerHeight()));
     $('#nav-fold-arraw-upper').addClass('nav-arrow-up');
     $('#nav-fold-arraw-upper').removeClass('nav-arrow-down');
     remocon.addClass('hide');
+    localStorage.setItem('navremocon-hide', JSON.stringify(true));
 }
-function navRemoconShow() {
+function navRemoconShow(reverse) {
+    adjustNavRemoconStyle(reverse);
     $('#nav-remocon').css('bottom', 0);
     if(!$('#nav-slider').hasClass('fold')) {
         $('#nav-fold-arraw-upper').removeClass('nav-arrow-up');
         $('#nav-fold-arraw-upper').addClass('nav-arrow-down');
     }
     $('#nav-remocon').removeClass('hide');
+    localStorage.setItem('navremocon-hide', JSON.stringify(false));
 }
 function navRemoconFold(call_obj) {
     // when upper button pressed,
@@ -44,9 +50,9 @@ function navRemoconFold(call_obj) {
             navRemoconShow();
         } else {  // if remocon is shown,
             if(navRemoconSlider.hasClass('fold')){  // unfold remocon
-                navRemoconUnfoldItem();
+                navRemoconUnfoldItem(true);
             } else {  // fold remocon
-                navRemoconFoldItem();
+                navRemoconFoldItem(true);
             }
         }
     } else if(call_obj.id == 'nav-fold-lower-button') {  // when lower button is pressed, hide remocon
@@ -55,6 +61,8 @@ function navRemoconFold(call_obj) {
 }
 function adjustNavRemoconStyle(reverse){
     reverse = (reverse >= true);  // it makes number, bigger than 1, to 'true'
+    // if has 'fold' and reverse is F, border will be 0.
+    // if has 'fold' and reverse is T, border will be return to initial.
     var decision = $('#nav-slider').hasClass('fold');
     decision = decision^reverse;
     if(decision) {
@@ -62,6 +70,36 @@ function adjustNavRemoconStyle(reverse){
     } else {
         $($('#nav-buttons').children()[1]).css('border-bottom', '');
     }
+}
+function reloadNavRemocon() {
+    var navFold = JSON.parse(localStorage.getItem('navremocon-fold'));
+    var navHide = JSON.parse(localStorage.getItem('navremocon-hide'));
+    $('#nav-remocon, #nav-slider, #nav-buttons').each(function(){
+        $(this).addClass('notransition');
+    });
+    if(navFold) {
+        navRemoconFoldItem();
+    } else {
+        navRemoconUnfoldItem();
+    }
+    if(navHide) {
+        navRemoconHide();
+    } else {
+        navRemoconShow();
+    }
+    // [Notice]
+    // We expect animation won't be work temporally by adding class 'notransition'.
+    // But It will be animated. Because the modern browsers are caching styling until javascript finish excuting.
+    // And then all styling will be in single css reflow.
+    // So, we NEED FORCE REFLOW while javascript code are running.
+    // Additionally, Timeout does not have compatibiltiy for various browsers.
+    // This Reference said.
+    // https://stackoverflow.com/questions/11131875/what-is-the-cleanest-way-to-disable-css-transition-effects-temporarily
+    // And like below code, Calling 'offsetHeight' will make reflow forcely (reference said maybe).
+    $('#nav-remocon, #nav-slider, #nav-buttons').each(function(){
+        $(this)[0].offsetHeight;  // Important! Do reflow, forcely.
+        $(this).removeClass('notransition');
+    });
 }
 function navRemoconGoToTop() {
     document.body.scrollTop = 0;
@@ -86,7 +124,7 @@ function tocFoldFolder(call_obj) {
 $(document).ready(function() {
     // [0] Run some codes immediately after complete page load
     // It may need to style some html..
-    adjustNavRemoconStyle();
+    reloadNavRemocon();
 
     // [1] Sidebar Toggle to (un)fold Submenu
     $('.nav__sub-title').click(function(){
