60dd36fb88c2a9a6019bd96c49a0106c3498ce7e
seungwubaek <whiteseungwu@gmail.com>
2020-12-14 20:28:22 +0900

    [Left Sidebar] Change font size and other styles and Replace item height css definition to using javascript for transition.

--- assets/js/customs.js
+++ assets/js/customs.js
@@ -163,11 +163,18 @@ $(document).ready(function() {
     $('.nav__sub-title-dropdownicon').click(function(){
         var $root_parent = $($(this).closest('li')[0]);
         var $navItems = $($root_parent.find('.nav__item-children')[0]).children();
-        if ($navItems.length > 0) {  // children이 있을때만 (+/-) 아이콘 표시를 위한 <span> 태그 하나 더 생김
-            s = $(this).children('i');
+        var numItems = $navItems.length
+        if (numItems > 0) {  // children이 있을때만 (+/-) 아이콘 표시를 위한 <span> 태그 하나 더 생김
+            var s = $(this).children('i');
             s.toggleClass('fa-plus');
             s.toggleClass('fa-minus');
-            $root_parent.children('ul').toggleClass('show');
+            var itemSet = $root_parent.children('ul')
+            itemSet.toggleClass('show');
+            if(itemSet.hasClass('show')){
+                var h = itemSet.children('li:first').height();
+                var child_h = numItems * h;
+                itemSet.css('max-height', child_h);
+            } else { itemSet.css('max-height', '') }
         }
     })
 
