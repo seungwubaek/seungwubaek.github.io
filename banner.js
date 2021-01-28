218417c9571b61e622f86b51bbc69be230397e9e
seungwubaek <whitesw01@gmail.com>
2021-01-28 10:40:35 +0900

    [banner.js] Add banner.js, include me, to main.min.js and customs.min.js

--- banner.js
+++ banner.js
@@ -1,3 +1,4 @@
+const authorName = "Sammy Baek";
 const fs = require("fs");
 const pkg = require("./package.json");
 const filename = "assets/js/main.min.js";
@@ -7,10 +8,12 @@ const dateObj = new Date();
 const date = `${dateObj.getFullYear()}-${padStart(
   dateObj.getMonth() + 1
 )}-${padStart(dateObj.getDate())}`;
+const customBanner =
+` * Customized Jekyll Theme by Sammy Baek
+ * Copyright 2020-${dateObj.getFullYear()} ${authorName}. All rights reserved.`;
 const banner = `/*!
- * Customized Jekyll Theme by Sammy Baek
- * Copyright 2020 Sammy Baek. All rights reserved.
- * with
+${customBanner}
+ * Powered by
  * Minimal Mistakes Jekyll Theme ${pkg.version} by ${pkg.author}
  * Copyright 2013-2020 Michael Rose - mademistakes.com | @mmistakes
  * Licensed under ${pkg.license}
@@ -19,4 +22,11 @@ const banner = `/*!
 
 if (script.slice(0, 3) != "/**") {
   fs.writeFileSync(filename, banner + script);
+}
+
+/* customs js */
+const filename2 = "assets/js/customs.min.js";
+const script2 = fs.readFileSync(filename2);
+if (script2.slice(0, 3) != "/**") {
+  fs.writeFileSync(filename2, banner + script2);
 }
