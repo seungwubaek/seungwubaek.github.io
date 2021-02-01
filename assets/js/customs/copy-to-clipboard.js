d6a5ed8b65928a4a50e893c1c2da7af4bf03c0a1
seungwubaek <whitesw01@gmail.com>
2021-02-01 17:10:28 +0900

    [Copy to Clipboard] Fix msg on CTC when copy url to clipboard.

--- assets/js/customs/copy-to-clipboard.js
+++ assets/js/customs/copy-to-clipboard.js
@@ -2,7 +2,7 @@
 $(function(){
     var clipboard = new ClipboardJS('.header-link');
     clipboard.on('success', function(e) {
-        simpleNotice.show('클립보드에 복사되었습니다.');
+        simpleNotice.show('URL을 클립보드에 복사하였습니다.');
         e.clearSelection();
     });
     // clipboard.on('error', function(e) {
