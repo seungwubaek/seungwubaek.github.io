2cd56433f4275609462a261dba4901f74797fd25
seungwubaek <whiteseungwu@gmail.com>
2021-02-25 21:50:40 +0900

    [Magnific-Popup] Add magnific-popup for post image.

---
+++ assets/js/customs/magnific-popup.js
@@ -0,0 +1,25 @@
+$(function() {
+  // Magnific-Popup options
+  $('.img-popup').magnificPopup({
+    delegate: 'img',
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      tError: '<a href="%url%">Image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.parent().data('title');
+			}
+		},
+    mainClass: 'mfp-zoom-in',
+    callbacks: {
+      elementParse: function(item) {
+        item.src = item.el.attr('src');
+      }
+    }
+  });
+});
